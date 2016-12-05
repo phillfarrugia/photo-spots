acf2096f99b0dabcc469226ff40830aab3d1a2fc
Phill Farrugia <me@phillfarrugia.com>
2016-12-06 10:09:42 +1100

    Creates PhotoSpotsCore framework

---
+++ PhotoSpotsCore/PhotoSpotsCore.h
@@ -0,0 +1,19 @@
+//
+//  PhotoSpotsCore.h
+//  PhotoSpotsCore
+//
+//  Created by Phill Farrugia on 6/12/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoSpotsCore.
+FOUNDATION_EXPORT double PhotoSpotsCoreVersionNumber;
+
+//! Project version string for PhotoSpotsCore.
+FOUNDATION_EXPORT const unsigned char PhotoSpotsCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoSpotsCore/PublicHeader.h>
+
+
