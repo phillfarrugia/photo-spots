72262042fa933fb52a37d4d4c509ba3883771b55
Phill Farrugia <me@phillfarrugia.com>
2016-12-06 10:08:32 +1100

    Creates initial Xcode project

---
+++ PhotoSpots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoSpots
+//
+//  Created by Phill Farrugia on 6/12/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
