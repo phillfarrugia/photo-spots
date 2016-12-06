23958c8cec8aa88ec5f340969182da25f48ed9b4
Phill Farrugia <me@phillfarrugia.com>
2016-12-06 12:01:03 +1100

    Adds reusable class

---
+++ PhotoSpotsCore/Utilities/Reusable.swift
@@ -0,0 +1,51 @@
+//
+//  Reusable.swift
+//  PhotoSpots
+//
+//  Created by Phill Farrugia on 6/12/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+public protocol Reusable: class {
+    static var reuseIdentifier: String { get }
+    static var nib: UINib? { get }
+}
+
+public extension Reusable {
+    static var reuseIdentifier: String { return String(describing: self) }
+    static var nib: UINib? { return UINib(nibName: String(describing: self), bundle: nil) }
+}
+
+public extension UITableView {
+    func registerReusableCell<T: UITableViewCell>(_: T.Type) where T: Reusable {
+        if let nib = T.nib {
+            self.register(nib, forCellReuseIdentifier: T.reuseIdentifier)
+        } else {
+            self.register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
+        }
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(indexPath: IndexPath) -> T where T: Reusable {
+        return self.dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as! T
+    }
+    
+    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>() -> T? where T: Reusable {
+        return self.dequeueReusableHeaderFooterView(withIdentifier: T.reuseIdentifier) as! T?
+    }
+}
+
+public extension UICollectionView {
+    func registerReusableCell<T: UICollectionViewCell>(_: T.Type) where T: Reusable {
+        if let nib = T.nib {
+            self.register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
+        } else {
+            self.register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+        }
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(indexPath: IndexPath) -> T where T: Reusable {
+        return self.dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as! T
+    }
+}
